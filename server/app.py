9b617e909126b3f2bbcdea7f77dfb29eacb616b7
+ <+>
2024-07-13 22:39:06 +0300

    changes

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 
 from flask import Flask, jsonify, request, make_response
@@ -16,7 +17,6 @@ db.init_app(app)
 
 api = Api(app)
 
-
 class Plants(Resource):
 
     def get(self):
@@ -24,6 +24,7 @@ class Plants(Resource):
         return make_response(jsonify(plants), 200)
 
     def post(self):
+
         data = request.get_json()
 
         new_plant = Plant(
@@ -37,19 +38,34 @@ class Plants(Resource):
 
         return make_response(new_plant.to_dict(), 201)
 
-
 api.add_resource(Plants, '/plants')
-
 
 class PlantByID(Resource):
 
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        data = request.get_json()
+        plant = Plant.query.filter_by(id=id).first()
+        for attr in data:
+            setattr(plant, attr, data[attr])
 
+        db.session.add(plant)
+        db.session.commit()
+
+        return make_response(plant.to_dict(), 200)
+    
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.add(plant)
+        db.session.commit()
+
+        return make_response('', 204)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
-
+        
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
